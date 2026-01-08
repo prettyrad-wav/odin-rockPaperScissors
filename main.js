7e6bbfd48ab184cb4f4728d2c800c436c72d0b3a
Nick Hobbs <officialkinggnarles@gmail.com>
2026-01-08 17:41:36 -0500

    Write the logic to play the full game, five rounds

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-console.log("Hello World!")
-
-function getComputerChoice(){
+    function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * (4 - 1) + 1);
     if (computerChoice === 1) {
         return("rock")
@@ -8,41 +6,57 @@ function getComputerChoice(){
         return("paper")        
     } else {
         return("scissors")
-    }
-}
-//console.log(getComputerChoice())
+    }}
 
-function getHumanChoice(){
+    function getHumanChoice(){
     let input = prompt("Rock, Paper, Scissors","Shoot!");
     let humanChoice = input.toLowerCase();
     return(humanChoice);
-}
-//console.log(getHumanChoice())
+    }
+    
+    let humanScore = 0;
+    let computerScore = 0;
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(myChoice, compChoice){
+    function playRound(myChoice, compChoice){
     myChoice = getHumanChoice();
     compChoice = getComputerChoice();
+
     if (myChoice === "rock" && compChoice === "scissors") {
         humanScore++
-        return(`You win! Rock beats scissors! You have ${humanScore} wins and ${computerScore} losses.`)
+        return(`You win this round! Rock beats scissors! You have ${humanScore} wins and ${computerScore} losses.`)
     } else if (myChoice == "scissors" && compChoice === "paper") {
         humanScore++
-        return(`You win! Scissors beats paper! You have ${humanScore} wins and ${computerScore} losses.`)
+        return(`You win this round! Scissors beats paper! You have ${humanScore} wins and ${computerScore} losses.`)
     } else if (myChoice === "paper" && compChoice == "rock") {
         humanScore++
-        return(`You win! Paper beats rock! You have ${humanScore} wins and ${computerScore} losses.`)
+        return(`You win this round! Paper beats rock! You have ${humanScore} wins and ${computerScore} losses.`)
     } else if (myChoice === "rock" && compChoice === "paper"){
         computerScore++
-        return(`You lose! Paper beats rock! you have ${humanScore} wins and ${computerScore} losses.`)
+        return(`You lose this round! Paper beats rock! you have ${humanScore} wins and ${computerScore} losses.`)
     } else if (myChoice === "paper" && compChoice === "scissors"){
         computerScore++
-        return(`You lose! Scissors beats paper! you have ${humanScore} wins and ${computerScore} losses.`)
+        return(`You lose this round! Scissors beats paper! you have ${humanScore} wins and ${computerScore} losses.`)
     } else if (myChoice === "scissors" && compChoice === "rock"){
         computerScore++
-        return(`You lose! Rock beats scissors! you have ${humanScore} wins and ${computerScore} losses.`)
+        return(`You lose this round! Rock beats scissors! you have ${humanScore} wins and ${computerScore} losses.`)
+    } else {
+        return(`Draw! the score remains ${humanScore} to ${computerScore}.`)
     }
+    }
+
+function playGame(){
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    if (humanScore > computerScore) {
+        return("You win! Refresh the page to play again!")
+    } else if (humanScore < computerScore) {
+        return("You lose! Refresh the page to try again!")
+    } else {
+        return("It's a tie! refresh the page to play again!")
+    }
+
 }
-console.log(playRound())
+console.log(playGame())
